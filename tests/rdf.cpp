7cbcbc2f85fdc69a4a932d97d86d4b825f60c9fe
David Brooks <dave@bcs.co.nz>
2014-11-17 21:12:39 +1300

    More tests.

--- tests/rdf.cpp
+++ tests/rdf.cpp
@@ -36,7 +36,11 @@ int main(void)
   )" ;
 
   std::string xml = R"(
-<rdf:RDF xmlns:dcterms="http://purl.org/dc/terms/" xmlns:map="http://www.biosignalml.org/ontologies/2011/02/mapping#" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#" xmlns="http://www.biosignalml.org/mappings/">
+<rdf:RDF xmlns:dcterms="http://purl.org/dc/terms/"
+         xmlns:map="http://www.biosignalml.org/ontologies/2011/02/mapping#"
+         xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+         xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"
+         xmlns="http://www.biosignalml.org/mappings/">
   <rdf:Description rdf:about="http://www.biosignalml.org/mappings/comment">
     <map:property rdf:resource="http://www.w3.org/2000/01/rdf-schema#comment"/>
   </rdf:Description>
@@ -70,8 +74,32 @@ int main(void)
 //  g.parseString(turtle, rdf::Graph::Format::TURTLE) ;
 //  g.parseString(xml, rdf::Graph::Format::RDFXML, "http://base.org") ;
 //  g.parseResource("file:///Users/dave/biosignalml/aobject/build/test.xml", rdf::Graph::Format::RDFXML) ;
-  g.parseResource("test.xml", rdf::Graph::Format::RDFXML) ;
+//  g.parseResource("test.xml", rdf::Graph::Format::RDFXML) ;
+
+  g.parseResource("http://demo.biosignalml.org/physiobank/mitdb/102/signal/ECG1") ;
+
 
   std::cout << g.serialise(rdf::Graph::Format::TURTLE) << std::endl ;
+
+
+  rdf::Statement in1(rdf::URI("http://www.biosignalml.org/mappings/comment"),
+                    rdf::URI("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"),
+                    rdf::URI("http://www.biosignalml.org/ontologies/2011/02/mapping#Mapping")) ;
+  rdf::Statement out1(rdf::URI("http://www.biosignalml.org/mappings/comment"),
+                    rdf::URI("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"),
+                    rdf::URI("http://www.biosignalml.org/ontologies/2011/02/mapping#xx")) ;
+
+  rdf::Statement in2(rdf::URI("http://www.biosignalml.org/mappings/comment"),
+                    rdf::URI("http://www.w3.org/2000/01/rdf-schema#comment"),
+                    rdf::Literal("comment")) ;
+  rdf::Statement out2(rdf::URI("http://www.biosignalml.org/mappings/comment"),
+                    rdf::URI("http://www.w3.org/2000/01/rdf-schema#label"),
+                    rdf::Literal("comment")) ;
+
+
+  std::cout << "I1: " << g.contains(in1)
+          << "  O1: " << g.contains(out1)
+          << "  I2: " << g.contains(in2)
+          << "  O2: " << g.contains(out2) << std::endl ;
 
   }
