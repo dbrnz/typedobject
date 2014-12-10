0257dcd39ebe48576dd91e1656a759049baa28bb
David Brooks <dave@bcs.co.nz>
2014-12-11 12:17:38 +1300

    Rework RDF interface to more tightly wrap Sord's C++ classes -- more abstraction would mean just using sord's C library.

--- src/rdf/rdf.cpp
+++ src/rdf/rdf.cpp
@@ -14,7 +14,7 @@
 
 //**************************************************************************//
 
-static Sord::World& sordWorld()
+static Sord::World &sordWorld()
 /*---------------------------*/
 {
   static Sord::World instance ;
@@ -36,44 +36,15 @@ static raptor_world *raptorWorld(void)
 
 //**************************************************************************//
 
-rdf::Node::Node()
-/*-------------*/
+rdf::URI::URI()
+/*-----------*/
 : Sord::Node()
 {
   }
 
-rdf::Node::Node(Type t, const std::string& s)
-/*-----------------------------------------*/
-: Sord::Node(sordWorld(), t, s)
-{
-  }
-
-rdf::Node::Node(SordNode* node, bool copy)
-/*--------------------------------------*/
-: Sord::Node(sordWorld(), node, copy)
-{
-  }
-
-
-SordNode *rdf::Node::sord_node_from_serd_node(
-/*------------------------------------------*/
-  const SerdNode* node, const SerdNode *type, const SerdNode *lang)
-{
-  return ::sord_node_from_serd_node(sordWorld().c_obj(), sordWorld().prefixes().c_obj(), node, type, lang) ;
-  }
-
-
-//**************************************************************************//
-
-rdf::URI::URI()
-/*-----------*/
-: rdf::Node()
-{
-  }
-
-rdf::URI::URI(const std::string& s)
-/*-------------------------------*/
-: rdf::Node(rdf::Node::URI, s)
+rdf::URI::URI(const std::string &uri)
+/*---------------------------------*/
+: Sord::Node(sordWorld(), Sord::Node::URI, uri)
 {
   }
 
@@ -82,64 +53,65 @@ rdf::URI::URI(const std::string& s)
 
 rdf::BNode::BNode(void)
 /*-------------------*/
-: rdf::Node(rdf::Node::BLANK, "")
+: Sord::Node(sordWorld(), Sord::Node::BLANK, "")
 {
   }
 
-rdf::BNode::BNode(const std::string& s)
+rdf::BNode::BNode(const std::string &s)
 /*-----------------------------------*/
-: rdf::Node(rdf::Node::BLANK, s)
+: Sord::Node(sordWorld(), Sord::Node::BLANK, s)
 {
   }
+
 
 //**************************************************************************//
 
-rdf::Literal::Literal(const std::string& s)
-/*---------------------------------------*/
-: rdf::Node(rdf::Node::LITERAL, s)
+rdf::Literal::Literal(const std::string &s)
+/*-------------------------------------------*/
+: Sord::Node(sordWorld(), Sord::Node::LITERAL, s)
 {
   }
 
 
-rdf::Literal::Literal(const std::string& s, const rdf::URI &datatype)
+rdf::Literal::Literal(const std::string &s, const rdf::URI &datatype)
 /*-----------------------------------------------------------------*/
-: rdf::Node(sord_datatype_node(s, datatype), false)
+: Sord::Node(sordWorld(), sord_datatype_node(s, datatype), false)
 {
   }
 
-rdf::Literal::Literal(const std::string& s, const std::string &language)
+rdf::Literal::Literal(const std::string &s, const std::string &language)
 /*--------------------------------------------------------------------*/
-: rdf::Node(sord_language_node(s, language), false)
+: Sord::Node(sordWorld(), sord_language_node(s, language), false)
 {
   }
 
 rdf::Literal::Literal(double d, unsigned frac_digits)
 /*-------------------------------------------------*/
-: rdf::Node(sord_decimal_node(d, frac_digits), false)
+: Sord::Node(sordWorld(), sord_decimal_node(d, frac_digits), false)
 {
   }
 
 rdf::Literal::Literal(int64_t i)
 /*----------------------------*/
-: rdf::Node(sord_integer_node(i), false)
+: Sord::Node(sordWorld(), sord_integer_node(i), false)
 {
   }
 
 
-SordNode *rdf::Literal::sord_datatype_node(const std::string& s, const rdf::URI &datatype)
+SordNode *rdf::Literal::sord_datatype_node(const std::string &s, const rdf::URI &datatype)
 /*--------------------------------------------------------------------------------------*/
 {
   const SerdNode val = serd_node_from_string(SERD_LITERAL, (const uint8_t*)s.c_str()) ;
   const SerdNode *type = sord_node_to_serd_node(datatype.c_obj()) ;
-  return rdf::Node::sord_node_from_serd_node(&val, type, NULL) ;
+  return sord_node_from_serd_node(&val, type, NULL) ;
   }
 
-SordNode *rdf::Literal::sord_language_node(const std::string& s, const std::string &language)
+SordNode *rdf::Literal::sord_language_node(const std::string &s, const std::string &language)
 /*-----------------------------------------------------------------------------------------*/
 {
   const SerdNode val = serd_node_from_string(SERD_LITERAL, (const uint8_t*)s.c_str()) ;
   const SerdNode lang = serd_node_from_string(SERD_LITERAL, (const uint8_t*)language.c_str()) ;
-  return rdf::Node::sord_node_from_serd_node(&val, NULL, &lang) ;
+  return sord_node_from_serd_node(&val, NULL, &lang) ;
   }
 
 SordNode *rdf::Literal::sord_decimal_node(double d, unsigned frac_digits)
@@ -147,7 +119,7 @@ SordNode *rdf::Literal::sord_decimal_node(double d, unsigned frac_digits)
 {
   const SerdNode val = serd_node_new_decimal(d, frac_digits) ;
   const SerdNode type = serd_node_from_string(SERD_URI, (const uint8_t*)SORD_NS_XSD "decimal") ;
-  return rdf::Node::sord_node_from_serd_node(&val, &type, NULL) ;
+  return sord_node_from_serd_node(&val, &type, NULL) ;
   }
 
 SordNode *rdf::Literal::sord_integer_node(int64_t i)
@@ -155,13 +127,49 @@ SordNode *rdf::Literal::sord_integer_node(int64_t i)
 {
   const SerdNode val = serd_node_new_integer(i) ;
   const SerdNode type = serd_node_from_string(SERD_URI, (const uint8_t*)SORD_NS_XSD "integer") ;
-  return rdf::Node::sord_node_from_serd_node(&val, &type, NULL) ;
+  return sord_node_from_serd_node(&val, &type, NULL) ;
   }
+
+SordNode *rdf::Literal::sord_node_from_serd_node(
+/*---------------------------------------------*/
+  const SerdNode *node, const SerdNode *type, const SerdNode *lang)
+{
+  return ::sord_node_from_serd_node(sordWorld().c_obj(), sordWorld().prefixes().c_obj(), node, type, lang) ;
+  }
+
+
+//**************************************************************************//
+
+rdf::Namespace::Namespace(const std::string &name, const std::string &uri)
+/*---------------------------------------------------------------------*/
+: m_name(name), m_uri(uri)
+{
+  }
+
+const rdf::URI rdf::Namespace::make_URI(const std::string &suffix) const
+/*---------------------------------------------------------------------*/
+{
+  return rdf::URI(m_uri.to_string() + suffix) ;
+  }
+
+
+rdf::Literal rdf::Namespace::name(void) const
+/*------------------------------------------*/
+{
+  return m_name ;
+  }
+
+rdf::URI rdf::Namespace::uri(void) const
+/*-------------------------------------*/
+{
+  return m_uri ;
+  }
+
 
 //**************************************************************************//
 
 rdf::Statement::Statement(const rdf::Node &s, const rdf::Node &p, const rdf::Node &o)
-/*-------------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------------*/
 {
   quad[SORD_SUBJECT] = s.c_obj() ;
   quad[SORD_PREDICATE] = p.c_obj() ;
@@ -169,25 +177,20 @@ rdf::Statement::Statement(const rdf::Node &s, const rdf::Node &p, const rdf::Nod
   quad[SORD_GRAPH] = NULL ;
   }
 
-rdf::Statement::Statement(const rdf::URI &s, const rdf::URI &p, const rdf::Node &o)
-/*-------------------------------------------------------------------------------*/
-: Statement((const rdf::Node &)s, (const rdf::Node &)p, o)
-{
-  }
-
-rdf::Statement::Statement(const rdf::BNode &s, const rdf::URI &p, const rdf::Node &o)
-/*---------------------------------------------------------------------------------*/
-: Statement((const rdf::Node &)s, (const rdf::Node &)p, o)
-{
-  }
-
 
 //**************************************************************************//
 
-rdf::Graph::Graph(const URI &p_uri)
-/*-------------------------------*/
+rdf::Graph::Graph()
+/*---------------*/
 : m_model(sord_new(sordWorld().c_obj(), SORD_SPO | SORD_OPS, false)),
-  m_uri(p_uri)
+  m_uri()
+{
+  }
+
+rdf::Graph::Graph(const std::string &uri)
+/*-------------------------------------*/
+: m_model(sord_new(sordWorld().c_obj(), SORD_SPO | SORD_OPS, false)),
+  m_uri(uri)
 {
   }
 
@@ -294,7 +297,7 @@ void rdf::Graph::parseResource(
 /*---------------------------*/
   const std::string &resource, const rdf::Graph::Format format, const std::string &base)
 {
-  uint8_t* file_path = get_file_from_uri_string((const uint8_t*)resource.c_str()) ;
+  uint8_t *file_path = get_file_from_uri_string((const uint8_t*)resource.c_str()) ;
 
   if ((format != Format::TURTLE && format != Format::NTRIPLES) || file_path == NULL) {  // Use Raptor...
     std::string base_str = (base != "") ? base : resource ;
@@ -312,7 +315,7 @@ void rdf::Graph::parseResource(
     raptor_free_parser(parser) ;
     }
   else {
-    SerdReader* reader = sord_new_reader(m_model, sordWorld().prefixes().c_obj(),
+    SerdReader *reader = sord_new_reader(m_model, sordWorld().prefixes().c_obj(),
                                          (format == Format::TURTLE) ? SERD_TURTLE : SERD_NTRIPLES,
                                          NULL) ;
     serd_reader_read_file(reader, file_path) ;
@@ -326,7 +329,7 @@ void rdf::Graph::parseString(
   const std::string &source, const rdf::Graph::Format format, const std::string &base)
 {
   if (format == Format::TURTLE || format == Format::NTRIPLES) {  // Use sord
-    SerdReader* reader = sord_new_reader(m_model, sordWorld().prefixes().c_obj(),
+    SerdReader *reader = sord_new_reader(m_model, sordWorld().prefixes().c_obj(),
                                          (format == Format::TURTLE) ? SERD_TURTLE : SERD_NTRIPLES,
                                          NULL) ;
     serd_reader_read_string(reader, (const uint8_t*)source.c_str()) ;
@@ -386,7 +389,7 @@ static raptor_term *raptor_term_from_sord_node(const SordNode *node)
 
 std::string rdf::Graph::serialise(
 /*-------------------------------*/
-  const rdf::Graph::Format format, const std::string &base, const rdf::Prefixes &prefixes)
+  const rdf::Graph::Format format, const std::string &base, const std::list<rdf::Namespace> &prefixes)
 {
   std::string result = "" ;
   const uint8_t *base_str = (base != "") ? (const uint8_t*)base.c_str()
@@ -396,16 +399,16 @@ std::string rdf::Graph::serialise(
     SerdURI base_uri = SERD_URI_NULL ;
     if (base_str) serd_uri_parse(base_str, &base_uri) ;
 
-    SerdWriter* writer = serd_writer_new((format == Format::TURTLE) ? SERD_TURTLE : SERD_NTRIPLES,
+    SerdWriter *writer = serd_writer_new((format == Format::TURTLE) ? SERD_TURTLE : SERD_NTRIPLES,
                                          (SerdStyle)(SERD_STYLE_ABBREVIATED | SERD_STYLE_RESOLVED | SERD_STYLE_CURIED),
                                          sordWorld().prefixes().c_obj(),
                                          &base_uri,
                                          Sord::string_sink, &result) ;
     serd_env_foreach(sordWorld().prefixes().c_obj(), (SerdPrefixSink)serd_writer_set_prefix, writer) ;
-    serd_env_foreach(prefixes.c_obj(), (SerdPrefixSink)serd_writer_set_prefix, writer) ;
-
+    for (const auto prefix : prefixes) {
+      serd_writer_set_prefix(writer, prefix.name().to_serd_node(), prefix.uri().to_serd_node()) ;
+      }
     sord_write(m_model, writer, 0) ;
-    
     serd_writer_free(writer) ;
     }
   else {                                   // Use Raptor
@@ -414,9 +417,10 @@ std::string rdf::Graph::serialise(
 
     raptor_serializer *serialiser = raptor_new_serializer(raptorWorld(), get_raptor_format(format)) ;
 //    if (serialiser == NULL) throw Exception("Unable to create graph serialiser") ;
-
     serd_env_foreach(sordWorld().prefixes().c_obj(), (SerdPrefixSink)set_raptor_prefix, serialiser) ;
-    serd_env_foreach(prefixes.c_obj(), (SerdPrefixSink)set_raptor_prefix, serialiser) ;
+    for (auto prefix : prefixes) {
+      set_raptor_prefix((SerdWriter *)serialiser, prefix.name().to_serd_node(), prefix.uri().to_serd_node()) ;
+      }
 
     size_t length = 0 ;
     unsigned char *serialised ;
@@ -451,7 +455,6 @@ const rdf::URI &rdf::Graph::getUri(void) const
   return m_uri ;
   }
 
-
 const bool rdf::Graph::contains(const Statement &statement) const
 /*-------------------------------------------------------------*/
 {
@@ -462,184 +465,25 @@ const bool rdf::Graph::contains(const Statement &statement) const
                   statement.quad[SORD_GRAPH]) ;
   }
 
-const bool rdf::Graph::contains(const URI &s, const URI &p, const Node &o) const
-/*----------------------------------------------------------------------------*/
+const bool rdf::Graph::contains(const rdf::Node &s, const rdf::Node &p, const rdf::Node &o) const
+/*----------------------------------------------------------------------------------------------*/
 {
   return contains(rdf::Statement(s, p, o)) ;
   }
 
-const bool rdf::Graph::contains(const BNode &s, const URI &p, const Node &o) const
-/*------------------------------------------------------------------------------*/
+
+rdf::StatementIter rdf::Graph::getStatements(const rdf::Statement &pattern) const
+/*-----------------------------------------------------------------------------*/
 {
-  return contains(rdf::Statement(s, p, o)) ;
+  return Sord::Iter(sordWorld(), sord_find(m_model, pattern.quad)) ;
   }
 
-/***
-StatmentIterator getStatements(const Statement &pattern)
-
-   SordIter *statements = sord_find(m_model, pattern.quad) ;
-
-**/
-//**************************************************************************//
-
-rdf::Namespace::Namespace(const std::string &prefix, const std::string &uri)
-/*------------------------------------------------------------------------*/
-: m_prefix(prefix), m_uri(uri)
+rdf::StatementIter rdf::Graph::getStatements(const rdf::Node &s, const rdf::Node &p, const rdf::Node &o) const
+/*-----------------------------------------------------------------------------------------------------------*/
 {
-  }
-
-const rdf::URI rdf::Namespace::make_URI(const std::string &name) const
-/*------------------------------------------------------------------*/
-{
-  return rdf::URI(m_uri.to_string() + m_prefix) ;
+  return getStatements(rdf::Statement(s, p, o)) ;
   }
 
 
 //**************************************************************************//
 
-/******************
-class Graph(rdflib.graph.Graph):
-#===============================
-
-  def add_statements(self, statements):
-  #------------------------------------
-    '''
-    Add statements to the graph.
-
-    :param statements: A sequence of :class:`Statement`\s to add.
-    :type statements: iterator
-    '''
-    for s in statements: self.add(s)
-
-  def append_graph(self, graph):
-  #-----------------------------
-    '''
-    Add statements from another graph to this graph.
-
-    :param graph: A :class:`Graph` containing statements.
-    '''
-    if graph is not None:
-      for s in graph: self.add(s)
-
-
-  def has_resource(self, uri, rtype):
-  #----------------------------------
-    '''
-    Does the graph contain a resource of the given type?
-    '''
-    return self.contains(Statement(uri, RDF.type, rtype))
-
-  def get_statements(self, statement):
-  #-----------------------------------
-    '''
-    Get all matching statements in the graph.
-
-    :param statement: The statement to find. Some or all of the `subject`,
-      `predicate` or `object` attributes can be `None`, meaning they match any value.
-    :type statement: :class:`Statement`
-    :return: A sequence of :class:`Statement`\s.
-    '''
-    for s in self.triples(statement): yield Statement(*s)
-
-  def get_object(self, s, p):
-  #--------------------------
-    '''
-    Get the object of a (`subject`, `predicate`) pair. One or both of `subject`
-    and `predicate` may be `None`, meaning they match any value.
-
-    :param s: The `subject` of the statement.
-    :param p: The `predicate` of the statement.
-    :return: The `object` node if the statement (`subject`, `predicate`, `object`) is
-      in the graph, otherwise `None`.
-    :rtype: :class:`Node`
-    '''
-    return tuple(self.get_objects(s, p))[0]
-
-  def get_literal(self, s, p):
-  #----------------------------
-    '''
-    Get the object of a (`subject`, `predicate`) pair as a string if it is a
-    :class:`Literal`. One or both of `subject` and `predicate` may be `None`,
-    meaning they match any value.
-
-    :param s: The `subject` of the statement to lookup.
-    :param p: The `predicate` of the statement to lookup.
-    :return: The `object` as a string if the statement (`subject`, `predicate`, `object`)
-      is in the graph and `object` is a :class:`Literal`; as a :class:`Node` if it
-      exists and is not a Literal; otherwise `None`.
-    '''
-    l = self.get_object(s, p)
-    return l.as_string() if l else None
-
-  def get_subjects(self, p, o):
-  #----------------------------
-    '''
-    Get a sequence of subjects matching the pair (`predicate`, `object`). One or
-    both of `predicate` and `object` may be `None`, meaning they match any value.
-
-    :param p: The `predicate` of the statement to lookup.
-    :param o: The `object` of the statement to lookup.
-    :return: An iterator yielding a sequence of `subject` nodes with
-      (`subject`, `predicate`, `object`) statements in the graph.
-    :rtype: iterator
-    '''
-    if p and not isinstance(p, rdflib.term.URIRef): p = Resource(p)
-    return self.subjects(predicate=p, object=o)
-
-  def get_objects(self, s, p):
-  #---------------------------
-    '''
-    Get a sequence of objects matching the pair (`subject`, `predicate`). One or
-    both of `subject` and `predicate` may be `None`, meaning they match any value.
-
-    :param s: The `subject` of the statement to lookup.
-    :param p: The `predicate` of the statement to lookup.
-    :return: An iterator yielding a sequence of `object` nodes with
-      (`subject`, `predicate`, `object`) statements in the graph.
-    :rtype: iterator
-    '''
-    if s and not isinstance(s, rdflib.term.URIRef): s = Resource(s)
-    if p and not isinstance(p, rdflib.term.URIRef): p = Resource(p)
-    return self.objects(subject=s, predicate=p)
-
-  def get_literals(self, s, p):
-  #------------------------------
-    '''
-    Get a sequence of objects matching the pair (`subject`, `predicate`), with the
-    `object` as a string if it is a :class:`Literal`. One or both of `subject`
-    and `predicate` may be `None`, meaning they match any value.
-
-    :param s: The `subject` of the statement to lookup.
-    :param p: The `predicate` of the statement to lookup.
-    :return: An iterator yielding a sequence of strings and non-Literal :class:`Node`\s
-       for `object`\s with (`subject`, `predicate`, `object`) statements in the graph.
-    :rtype: iterator
-    '''
-    for v in self.get_objects(s, p): yield v.as_string()
-
-  def set_subject_property(self, s, p, o):
-  #---------------------------------------
-    """
-    Append the statement (s, p, o) to the graph after first removing
-    all statements with subject `s` and predicate `p`.
-    """
-    self.remove(Statement(s, p, None))
-    self.add(Statement(s, p, o))
-
-  def query(self, sparql):
-  #-----------------------
-    '''
-    Perform a SPARQL query against RDF statements in the graph.
-
-    :param sparql: The SPARQL query.
-    :type: str
-    :return: An iterator of the results from the query.
-    :rtype: :class:`QueryResults`
-    '''
-    try:
-      return super(Graph, self).query(sparql)
-    except Exception as msg:
-      logging.error('Graph query: %s', msg)
-    return [ ]
-
-******************/
