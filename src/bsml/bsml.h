ef4fc7283f6d059274e4c21981ec9151491051d9
David Brooks <dave@bcs.co.nz>
2014-12-31 12:46:41 +1300

    Use generated ontology definitions.

--- src/bsml/bsml.h
+++ src/bsml/bsml.h
@@ -1,19 +1,5 @@
 #ifndef BSML_BSML_H
 
-#include "rdf/rdf.h"
-#include "rdf/defs.h"
-
-namespace bsml {
-
-// bsml.h
-  class BSML {
-   public:
-    static const rdf::Namespace NS ;
-    static const rdf::URI Recording ;
-    static const rdf::URI dataset ;
-    } ;
-
-  } ;
-
+#include "ontology.h"
 
 #endif
