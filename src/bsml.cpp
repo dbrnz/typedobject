28a7ae249a315b02382a68a68c00e85ef91625b5
David Brooks <dave@bcs.co.nz>
2014-12-31 12:46:41 +1300

    Use generated ontology definitions.

--- src/bsml.cpp
+++
@@ -1,8 +0,0 @@
-#include "bsml.h"
-
-
-// bsml.cpp
-const rdf::Namespace bsml::BSML::NS("bsml", "http://www.biosignalml.org/ontologies/2011/04/biosignalml#") ;
-
-const rdf::URI bsml::BSML::Recording = BSML::NS.make_URI("Recording") ;
-const rdf::URI bsml::BSML::dataset = BSML::NS.make_URI("dataset") ;
