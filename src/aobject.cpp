abcd9f1af8c7ad0b35b767b27b02d3e5746450fe
David Brooks <dave@bcs.co.nz>
2015-01-03 14:05:18 +1300

    Remove temporary comment.

--- src/aobject.cpp
+++ src/aobject.cpp
@@ -35,19 +35,6 @@ namespace AObject {
           } while (!statements.next()) ;
         }
       }
-/***
-      for stmt in p_graph.getStatements(m_uri, None, None) {
-// For all rdfmaps going forwards... (that aren't empty URIs)
-          for metaclass in [getattr(cls, 'metaclass', None)
-                              for cls in self.__class__.__mro__ if cls != object]:
-
-            s, attr, v, fn = self.rdfmap.metadata(metaclass, stmt)
-            #logging.debug("%s: %s = '%s'", self.uri, attr, v)  ###
-            if attr is not None:
-              self._assign(attr, v, fn)
-              break
-      }
-**/
     }
 
 /*
