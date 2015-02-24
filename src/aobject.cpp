ee2f6817671ce5a8be4947a40d1d4be0ded6a63b
David Brooks <dave@bcs.co.nz>
2015-02-24 16:28:07 +1300

    Remove unused methods.

--- src/aobject.cpp
+++ src/aobject.cpp
@@ -93,35 +93,4 @@ namespace AObject {
     }
 
 
-  AObject *AObject::create_from_graph(const rdf::Graph &p_graph, const rdf::URI &p_uri)
-  /*---------------------------------------------------------------------------------*/
-  {
-
-/*    get type from graph: (uri a ?type)
-
-    look up registered types to get class creator
-
-    creator->create(uri)
-
-    (BSML::Recording, new RecordingFactory()) ;
-
-*/
-
-
-
-/*    AObject *self = new AObject(p_uri) ;   // But needs to be object with metaclass()
-                                           // matching rdf:type
-    self->addMetadata(p_graph) ;
-    self->setGraph(p_graph.getUri()) ;
-    return self ;
-    */ return NULL ;
-    }
-
-  void AObject::set_graph(const rdf::URI &p_graph)
-  /*--------------------------------------------*/
-  {
-
-    }
-
-
   } ;
