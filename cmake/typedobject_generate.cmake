4346e5e2cb56f8447af3fcf0b79f37b7977c78f8
David Brooks <dave@bcs.co.nz>
2015-03-10 22:12:51 +1300

    Use `include()` instead of `find_package()` to access the generation function. This allows the project to be built as a sub-project by using `add_subdirectory()`.

--- cmake/typedobject_generate.cmake
+++ cmake/typedobject_generate.cmake
@@ -1,13 +1,11 @@
-set(TYPEDOBJECT_SRC ${CMAKE_CURRENT_LIST_DIR}/../src)
-
 function(typedobject_generate generated_files header_file)
   get_filename_component(base ${header_file} NAME_WE)
   set(gen_file ${CMAKE_CURRENT_BINARY_DIR}/tobj_${base}.cpp)
   set(${generated_files} ${${generated_files}} ${gen_file} PARENT_SCOPE)
   add_custom_command(
     OUTPUT ${gen_file}
-    COMMAND python ${TYPEDOBJECT_SRC}/clang/generate.py
-                   -I${TYPEDOBJECT_SRC}
+    COMMAND python ${typedobject_DIR}/src/clang/generate.py
+                   -I${typedobject_DIR}/src
                    ${CMAKE_CURRENT_SOURCE_DIR}/${header_file}
                    ${gen_file}
     DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/${header_file}
