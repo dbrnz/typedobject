91aac2aca11df785b6cb930e1a44abd20dceb1ad
David Brooks <dave@bcs.co.nz>
2015-05-10 20:49:17 +0200

    Hide implementation details of `utils::Datetime` and `utils::Duration`.

--- src/utils.cpp
+++ src/utils.cpp
@@ -20,29 +20,226 @@
 
 #include "utils.h"
 
+#include <boost/date_time/posix_time/posix_time.hpp> // _types.hpp>
+
 #include <math.h>
 #include <sstream>
 
+namespace utils {
+
+  class DatetimeImpl
+  /*--------------*/
+  {
+   public:
+
+    DatetimeImpl()
+    : m_ptime(boost::date_time::not_a_date_time)
+    {
+      }
+
+    DatetimeImpl(const std::string &dt)
+    : m_ptime(boost::posix_time::time_from_string(dt))
+    {
+      }
+
+    std::string to_string(void) const
+    {
+      return boost::posix_time::to_iso_extended_string(m_ptime) ;
+      }
+
+    bool is_valid(void) const
+    {
+      return !m_ptime.is_special() ;
+      }
+
+   private:
+    boost::posix_time::ptime m_ptime ;
+    } ;
+
+
+  class DurationImpl
+  /*--------------*/
+  {
+   public:
+
+    DurationImpl()
+    : m_duration(boost::date_time::not_a_date_time)
+    {
+      }
+
+    DurationImpl(const std::string &dt, const bool strict=false)
+    {
+      const char *s = dt.c_str() ;
+      bool negative = (*s == '-') ;
+      if (negative) ++s ;
+
+      long days = 0 ;
+      long hours = 0 ;
+      long minutes = 0 ;
+      long seconds = 0 ;  long fraction = 0 ;  long decimals = 0 ;
+      boost::posix_time::time_duration td ;
+
+      if (strict || *s == 'P') {  // XSD duration format
+        if (*s != 'P') throw utils::TimeException("Invalid XSD duration: '" + dt + "'") ;
+        ++s ;
+        bool scanned = false ;
+        if (*s == 'T') ++s ;
+        else {
+          s = scan_integer(s, &days, 'D') ;
+          if (*s == 'T') ++s ;
+          else if (*s == '\0') scanned = true ;
+          else throw utils::TimeException("Invalid XSD duration: '" + dt + "'") ;
+          }
+        if (!scanned) {
+          s = this->scan_integer(s, &hours, 'H') ;
+          s = this->scan_integer(s, &minutes, 'M') ;
+          s = this->scan_decimal(s, &seconds, &fraction, &decimals, 'S') ;
+          }
+        }
+      else {                      // Assume seconds
+        s = this->scan_decimal(s, &seconds, &fraction, &decimals, '\0') ;
+        }
+      if (*s != '\0') throw utils::TimeException("Invalid duration: '" + dt + "'") ;
+      td = boost::posix_time::time_duration(24*days + hours, minutes, seconds,
+                                            fraction*boost::posix_time::time_duration::ticks_per_second()
+                                            / pow(10, decimals)) ;
+      m_duration = negative ? td.invert_sign() : td ;
+      }
+
+    std::string to_string(void) const
+    {
+      std::stringstream ss("") ;
+      if (!(m_duration.is_special() && m_duration.is_not_a_date_time())) {
+        boost::posix_time::time_duration dt ;
+        if (m_duration.is_negative()) {
+          ss << "-" ;
+          dt = m_duration.invert_sign() ;
+          }
+        else dt = m_duration ;
+        ss << "P" ;
+        long hours = dt.hours() ;
+        long mins = dt.minutes() ;
+        long secs = dt.seconds() ;
+        long frac = dt.fractional_seconds() ;
+        if (hours || mins || secs || frac) {
+          if (secs >= 60) {
+            mins += secs/60 ;
+            secs %= 60 ;
+            }
+          if (mins >= 60) {
+            hours += mins/60 ;
+            mins %= 60 ;
+            }
+          if (hours >= 24) {
+            ss << hours/24 << "D" ;
+            hours %= 24 ;
+            }
+          if (hours || mins || secs || frac) ss << "T" ;
+          if (hours) ss << hours << "H" ;
+          if (mins) ss << mins << "M" ;
+          if (secs || frac) {
+            ss << secs ;
+            if (frac) {
+              while ((frac % 10) == 0) frac /= 10 ;
+              ss << "." ;
+              ss << frac ;
+              }
+            ss << "S" ;
+            }
+          }
+        else ss << "T0S" ;
+        }
+      return ss.str() ;
+      }
+
+    bool is_valid(void) const
+    {
+      return !m_duration.is_special() ;
+      }
+
+   private:
+    boost::posix_time::time_duration m_duration ;
+
+    const char *scan_decimal(const char *s, long *number, long *fraction, long *places,
+                             const char terminator)
+    {
+      *number = 0 ;
+      *fraction = 0 ;
+      *places = 0 ;
+      if (isdigit(*s)) {
+        char *end ;
+        long i = strtol(s, &end, 10) ;
+        if (*end == terminator) {
+          *number = i ;
+          return terminator ? (end + 1) : end ;
+          }
+        else if (*end == '.') {
+          char *d = end + 1 ;
+          if (isdigit(*d)) {
+            long f = strtol(d, &end, 10) ;
+            if (*end == terminator) {
+              *number = i ;
+              *fraction = f ;
+              *places = (end - d) ;
+              return terminator ? (end + 1) : end ;
+              }
+            }
+          }
+        }
+      return s ;
+      }
+
+    const char *scan_integer(const char *s, long *result, const char terminator)
+    {
+      if (isdigit(*s)) {
+        char *end ;
+        long i = strtol(s, &end, 10) ;
+        if (*end == terminator) {
+          *result = i ;
+          return end + 1 ;
+          }
+        }
+      *result = 0 ;
+      return s ;
+      }
+
+    } ;
+
+  } ;
+
+
+
+
+utils::Datetime::Datetime()
+/*-----------------------*/
+: m_datetime(nullptr)
+{
+  }
 
 utils::Datetime::Datetime(const std::string &dt)
 /*--------------------------------------------*/
+: m_datetime(new DatetimeImpl(dt))
 {
-//  m_time = boost::posix_time::time_from_string(dt) ;
   }
 
 utils::Datetime::Datetime(const rdf::Node &node)
 /*--------------------------------------------*/
-: utils::Datetime(node.to_string())
 {
   if (!node.is_literal_type(rdf::XSD::dateTime.to_string().c_str()))
     throw utils::TimeException("Not a XSD dateTime: '" + node.to_string() + "'") ;
-//  m_time = boost::posix_time::time_from_string(node.to_string()) ;
+  m_datetime = new DatetimeImpl(node.to_string()) ;
+  }
+
+utils::Datetime::~Datetime()
+/*------------------------*/
+{
+  if (m_datetime != nullptr) delete m_datetime ;
   }
 
 std::string utils::Datetime::to_string(void) const
 /*----------------------------------------------*/
 {
-  return boost::posix_time::to_iso_extended_string(m_time) ;
+  return (m_datetime == nullptr) ? "" : m_datetime->to_string() ;
   }
 
 rdf::Literal utils::Datetime::to_literal(void)
@@ -54,147 +251,42 @@ rdf::Literal utils::Datetime::to_literal(void)
 bool utils::Datetime::is_valid(void) const
 /*--------------------------------------*/
 {
-  return !m_time.is_special() ;
+  return (m_datetime != nullptr) && m_datetime->is_valid() ;
   }
 
 
-const char *utils::Duration::scan_decimal(const char *s, long *number, long *fraction, long *places,
-/*------------------------------------------------------------------------------------------------*/
-                                          const char terminator)
-{
-  *number = 0 ;
-  *fraction = 0 ;
-  *places = 0 ;
-  if (isdigit(*s)) {
-    char *end ;
-    long i = strtol(s, &end, 10) ;
-    if (*end == terminator) {
-      *number = i ;
-      return terminator ? (end + 1) : end ;
-      }
-    else if (*end == '.') {
-      char *d = end + 1 ;
-      if (isdigit(*d)) {
-        long f = strtol(d, &end, 10) ;
-        if (*end == terminator) {
-          *number = i ;
-          *fraction = f ;
-          *places = (end - d) ;
-          return terminator ? (end + 1) : end ;
-          }
-        }
-      }
-    }
-  return s ;
-  }
 
-const char *utils::Duration::scan_integer(const char *s, long *result, const char terminator)
-/*-----------------------------------------------------------------------------------------*/
+
+utils::Duration::Duration()
+/*-----------------------*/
+: m_duration(nullptr)
 {
-  if (isdigit(*s)) {
-    char *end ;
-    long i = strtol(s, &end, 10) ;
-    if (*end == terminator) {
-      *result = i ;
-      return end + 1 ;
-      }
-    }
-  *result = 0 ;
-  return s ;
   }
 
 utils::Duration::Duration(const std::string &dt, const bool strict)
 /*---------------------------------------------------------------*/
+: m_duration(new DurationImpl(dt, strict))
 {
-  const char *s = dt.c_str() ;
-  bool negative = (*s == '-') ;
-  if (negative) ++s ;
-
-  long days = 0 ;
-  long hours = 0 ;
-  long minutes = 0 ;
-  long seconds = 0 ;  long fraction = 0 ;  long decimals = 0 ;
-  boost::posix_time::time_duration td ;
-
-  if (strict || *s == 'P') {  // XSD duration format
-    if (*s != 'P') throw utils::TimeException("Invalid XSD duration: '" + dt + "'") ;
-    ++s ;
-    bool scanned = false ;
-    if (*s == 'T') ++s ;
-    else {
-      s = scan_integer(s, &days, 'D') ;
-      if (*s == 'T') ++s ;
-      else if (*s == '\0') scanned = true ;
-      else throw utils::TimeException("Invalid XSD duration: '" + dt + "'") ;
-      }
-    if (!scanned) {
-      s = this->scan_integer(s, &hours, 'H') ;
-      s = this->scan_integer(s, &minutes, 'M') ;
-      s = this->scan_decimal(s, &seconds, &fraction, &decimals, 'S') ;
-      }
-    }
-  else {                      // Assume seconds
-    s = this->scan_decimal(s, &seconds, &fraction, &decimals, '\0') ;
-    }
-  if (*s != '\0') throw utils::TimeException("Invalid duration: '" + dt + "'") ;
-  td = boost::posix_time::time_duration(24*days + hours, minutes, seconds,
-                                        fraction*boost::posix_time::time_duration::ticks_per_second()
-                                        / pow(10, decimals)) ;
-  m_duration = negative ? td.invert_sign() : td ;
   }
 
 utils::Duration::Duration(const rdf::Node &node)
 /*--------------------------------------------*/
-: utils::Duration(node.to_string(), node.is_literal_type(rdf::XSD::dayTimeDuration.to_string().c_str()))
+: utils::Duration(node.to_string(),
+                  node.is_literal_type(rdf::XSD::dayTimeDuration.to_string().c_str()))
+
 {
   }
 
+utils::Duration::~Duration()
+/*------------------------*/
+{
+  if (m_duration != nullptr) delete m_duration ;
+  }
 
 std::string utils::Duration::to_string(void) const
 /*----------------------------------------------*/
 {
-  std::stringstream ss("") ;
-  if (!(m_duration.is_special() && m_duration.is_not_a_date_time())) {
-    boost::posix_time::time_duration dt ;
-    if (m_duration.is_negative()) {
-      ss << "-" ;
-      dt = m_duration.invert_sign() ;
-      }
-    else dt = m_duration ;
-    ss << "P" ;
-    long hours = dt.hours() ;
-    long mins = dt.minutes() ;
-    long secs = dt.seconds() ;
-    long frac = dt.fractional_seconds() ;
-    if (hours || mins || secs || frac) {
-      if (secs >= 60) {
-        mins += secs/60 ;
-        secs %= 60 ;
-        }
-      if (mins >= 60) {
-        hours += mins/60 ;
-        mins %= 60 ;
-        }
-      if (hours >= 24) {
-        ss << hours/24 << "D" ;
-        hours %= 24 ;
-        }
-      if (hours || mins || secs || frac) ss << "T" ;
-      if (hours) ss << hours << "H" ;
-      if (mins) ss << mins << "M" ;
-      if (secs || frac) {
-        ss << secs ;
-        if (frac) {
-          while ((frac % 10) == 0) frac /= 10 ;
-          ss << "." ;
-          ss << frac ;
-          }
-        ss << "S" ;
-        }
-      }
-    else ss << "T0S" ;
-    }
-  return ss.str() ;
+  return (m_duration == nullptr) ? "" : m_duration->to_string() ;
   }
 
 rdf::Literal utils::Duration::to_literal(void)
@@ -206,5 +298,6 @@ rdf::Literal utils::Duration::to_literal(void)
 bool utils::Duration::is_valid(void) const
 /*--------------------------------------*/
 {
-  return !m_duration.is_special() ;
+  return (m_duration != nullptr) && m_duration->is_valid() ;
   }
+
--- src/utils.h
+++ src/utils.h
@@ -25,7 +25,6 @@
 #include "rdf.h"
 #include "rdfdefs.h"
 
-#include <boost/date_time/posix_time/posix_time.hpp> // _types.hpp>
 
 #include <stdexcept>
 #include <string>
@@ -33,43 +32,48 @@
 
 namespace utils {
 
-  class TYPEDOBJECT_EXPORT TimeException : public std::range_error
-  /*------------------------------------------------------------*/
+  class DatetimeImpl ;           // Declare forward
+  class DurationImpl ;           // Declare forward
+
+
+  class TimeException : public std::range_error
+  /*-----------------------------------------*/
   {
    public:
     TimeException(const std::string &reason)
      : std::range_error(reason) { }
     } ;
 
+
   class TYPEDOBJECT_EXPORT Datetime
   /*------------------------------*/
   {
    public:
-    Datetime(): m_time(boost::date_time::not_a_date_time) { }
+    Datetime() ;
     Datetime(const std::string &dt) ;
     Datetime(const rdf::Node &node) ;
+    virtual ~Datetime() ;
     std::string to_string(void) const ;
     rdf::Literal to_literal(void) ;
     bool is_valid(void) const ;
    private:
-    boost::posix_time::ptime m_time ;
+    DatetimeImpl *m_datetime ;
     } ;
+
 
   class TYPEDOBJECT_EXPORT Duration
   /*-----------------------------*/
   {
    public:
-    Duration(): m_duration(boost::date_time::not_a_date_time) { }
+    Duration() ;
     Duration(const std::string &dt, const bool strict=false) ;
     Duration(const rdf::Node &node) ;
+    virtual ~Duration() ;
     std::string to_string(void) const ;
     rdf::Literal to_literal(void) ;
     bool is_valid(void) const ;
    private:
-    boost::posix_time::time_duration m_duration ;
-    static const char *scan_decimal(const char *s, long *number, long *fraction, long *places,
-                                    const char terminator) ;
-    static const char *scan_integer(const char *s, long *result, const char terminator) ;
+    DurationImpl *m_duration ;
     } ;
 
   } ;
