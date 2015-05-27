e2486e74a26e5d7d4482624916a4b8a2f514ea7e
David Brooks <dave@bcs.co.nz>
2015-05-27 20:44:31 +0200

    Add more tests.

--- tests/unittest.cpp
+++ tests/unittest.cpp
@@ -13,7 +13,21 @@ BOOST_AUTO_TEST_CASE(node)
 
 BOOST_AUTO_TEST_CASE(uri)
 {
-  std::string id = "http://example.org/id#1" ;
+  std::string id = "http://example.org/id/1" ;
   rdf::URI uri(id) ;
-  BOOST_WARN(id != uri.to_string()) ;
+  BOOST_CHECK(id == uri.to_string()) ;
+  }
+
+BOOST_AUTO_TEST_CASE(relative_uri)
+{
+  std::string base = "http://example.org/id/" ;
+  std::string id = "2" ;
+  rdf::URI uri(id, base) ;
+  BOOST_CHECK((base + id) == uri.to_string()) ;
+  }
+
+BOOST_AUTO_TEST_CASE(invalid_uri)
+{
+  rdf::URI uri("invalid") ;
+  BOOST_CHECK(uri.is_valid()) ;
   }
